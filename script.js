a25a3ca88d310ab7252d11ea8eb4cde13ba64dab
BrianU <brian.ulatowski@gmail.com>
2020-01-28 22:39:12 -0800

    added questions

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//var questions = require('questions');
 var questions = [
     {
         title: "Commonly used data types DO NOT include:",
@@ -7,71 +6,37 @@ var questions = [
     },
     {
         title: "The condition in an if / else statement is enclosed within ____.",
-        choices: ["1) quotes", "2) curly brackets", "3) parentheses", "4) square brackets"],
-        answer: "3) parentheses"
+        choices: ["1) quotes", "2) parentheses", "3) curly brackets", "4) square brackets"],
+        answer: "2) parentheses"
+    },
+    {
+        title: "Computer programming is ",
+        choices: ["1) a science", "2) an art", "3) both 1 and 2", "4) neither"],
+        answer: "2) an art"
+    },
+    {
+        title: "Which is a value that is written into the code of a program?",
+        choices: ["1) literal", "2) variable", "3) assignment statement", "4) operator"],
+        answer: "1) literal"
     },
 ];
 
+var currentQuestion = document.querySelector("#displayQuestion");
+var choiceA = document.querySelector("#choice0");
+var choiceB = document.querySelector("#choice1");
+var choiceC = document.querySelector("#choice2");
+var choiceD = document.querySelector("#choice3");
+var i = 0;
+var startClick = document.querySelector(".startBtn");
+var choiceClickA = document.getElementById("#choice0");
+var choiceClickB = document.getElementById("#choice1");
+var choiceClickC = document.getElementById("#choice2");
+var choiceClickD = document.getElementById("#choice3");
+var answer = "";
 
-
-function main() {
-
-    var startClick = document.querySelector(".startBtn");
-
-    startClick.addEventListener("click", function () {
-        quizTimer();
-        quiz();
-    });
-
-
-    var currentQuestion = document.querySelector("#displayQuestion");
-    var choiceA = document.querySelector("#choice0");
-    var choiceB = document.querySelector("#choice1");
-    var choiceC = document.querySelector("#choice2");
-    var choiceD = document.querySelector("#choice3");
-
-    function quiz() {
-        
-        //for (let i = 0; i < 4; i++) {
-
-        //}
-        //if (questionNumber === 4) {
-            
-        //}
-
-        //var questionNumber = 0;
-        //currentQuestion.textContent = (the current question here)      
-        
-        currentQuestion.textContent = questions[0].title
-        choiceA.textContent = questions [0].choices[0]
-        choiceB.textContent = questions [0].choices[1]
-        choiceC.textContent = questions [0].choices[2]
-        choiceD.textContent = questions [0].choices[3]
-
-        if () {
-            
-        }
-
-        //answerOptions.textContent = (current answer options here)
-        //process through the questions
-        //process through answer options
-        //process through the correct answer
-        //display first question and answer options on click
-        //function displayQuestion(questionNumber) { 
-        //currentQuestion.innerHTML = "Question: " + array[questionNumber];
-
-            
-            
-        //}
-        //displayQuestion();
-    }
-    
-
-//console.log("working");
-
+    var count = 60;
 
     function quizTimer() {
-        count = 60;
         var interval = setInterval(function () {
             document.querySelector("#count").innerHTML = "Remaining Time: " + count + "s";
             count--;
@@ -81,7 +46,58 @@ function main() {
             };
         }, 1000);
     };
-}
-main();
+
+   
+    function startQuiz() {
+        console.log("inside startquiz");
+        //if statement is to prevent user from keep clicking the- 
+        //start button and restarting timer after starting the quiz
+        if (count >= 59) {
+            startClick.textContent = "Question";
+            quizTimer();
+            quiz();
+        };
+    };
+    
+    console.log("wglhfuhwueh");
+    
+    function quiz() {
+
+            if (i === questions.length || count === -2) {
+                endQuiz();
+            }
+
+            currentQuestion.textContent=questions[i].title;
+            choiceA.textContent=questions[i].choices[0];
+            choiceA.setAttribute("value", questions[i].choices[0]);
+            choiceB.textContent=questions[i].choices[1];
+            choiceB.setAttribute("value", questions[i].choices[1]);
+            choiceC.textContent=questions[i].choices[2];
+            choiceC.setAttribute("value", questions[i].choices[2]);
+            choiceD.textContent=questions[i].choices[3];
+            choiceD.setAttribute("value", questions[i].choices[3]);
+            answer = questions[i].answer;
+
+            console.log(i);
+   
+    };
+    function checkAnswers(buttonObj) {
+        console.log("i got here");
+        console.log(this.value);
+        console.log(questions[i]);
+
+        if ((buttonObj.value) === questions[i].answer) {
+            document.querySelector("#confirmAnswer").innerHTML = "Correct!!";            
+        } else {
+            document.querySelector("#confirmAnswer").innerHTML = "Wrong!!";
+            count = count - 10;
+        } 
+        i++;
+        quiz();
+    };
+
+    function endQuiz() {
+        return;
+    };
 
 console.log("work");
